869f3fdd7bb2ac38176b83d5319b90cb5083fa5b
Felipe Sousa Pessina <f171214@ic.unicamp.br>
2018-04-08 08:47:23 -0300

    Easter egg

--- src/HomePage.js
+++ src/HomePage.js
@@ -4,32 +4,10 @@ import { Grid, Container, Input, Button, Dropdown, Menu, Image, Modal } from 'se
 import logo from './images/umbrella-car.svg';
 import feliz from './images/feliz.jpeg';
 import triste from './images/triste.jpeg';
-import { locationsLatLong } from './constants';
 
 const fetch = require('node-fetch');
 
 global.Headers = fetch.Headers;
-
-// export const getProbabilities = (name, email, phone, location, date, daysPredict) => ();
-
-
-// fetch('https://alagatudo.now.sh/predict', {
-//   method: 'POST',
-//   mode: 'no-cors',
-//   body: {
-//     name,
-//     email,
-//     phone,
-//     location,
-//     date,
-//     daysPredict
-//   }
-// })
-//   .then(res => res.json())
-//   .catch((err) => {
-//     console.log(err);
-//     return null;
-//   });
 
 const sectionForms = [
   {
@@ -43,18 +21,8 @@ const sectionForms = [
     type: 'Text'
   },
   {
-    id: 'telephone',
+    id: 'phone',
     name: 'Telefone',
-    type: 'Text'
-  },
-  {
-    id: 'predict_days',
-    name: 'Dias para Prever',
-    type: 'Text'
-  },
-  {
-    id: 'date',
-    name: 'Data',
     type: 'Text'
   },
   {
@@ -198,44 +166,37 @@ const locationOptions = [
 class HomePage extends Component {
   state = {
     selectorValue: null,
-    probability: true,
+    probability: false,
     open: false
   }
 
   buttonSubmit = async () => {
     this.setState({ open: true });
     if (document.getElementById('email')) {
-      console.log('dinossauro rex');
       const name = document.getElementById('name');
       const email = document.getElementById('email');
-      const telephone = document.getElementById('telephone');
-      const date = document.getElementById('date');
-      const predict_days = document.getElementById('predict_days');
+      const phone = document.getElementById('phone');
       const a = await fetch('https://alagatudo.now.sh/predict', {
         method: 'POST',
-        mode: 'no-cors',
         headers: {
           Accept: 'application/json',
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          date: '2018-04-08T20:30:33.304Z',
-          location: 'butanta',
-          name: 'Cristiano',
-          email: 'cristianochiaramelli@gmail.com'
+          date: new Date().toISOString(),
+          location: this.state.selectorValue,
+          name: name.value,
+          email: email.value,
+          phone
         })
-      }).then((res) => {
-        console.log('this is res', res);
-      }).catch((err) => {
-        console.log(err);
-      });
-      console.log('olar');
-      console.log(a);
+      }).then(res => res.json())
+        .catch((err) => {
+          console.log(err);
+        });
+      this.setState({ probability: a });
       name.value = '';
       email.value = '';
-      telephone.value = '';
-      date.value = '';
-      predict_days.value = '';
+      phone.value = '';
     }
   }
 
@@ -245,7 +206,7 @@ class HomePage extends Component {
      return (
        <div>
          <Helmet
-           title="ClassApp"
+           title="Alagaalagar.io"
            meta={[
           { name: 'description', content: 'Sample' },
           { name: 'keywords', content: 'sample, something' },
@@ -258,20 +219,20 @@ class HomePage extends Component {
            <Menu style={{ height: '72px', margin: '0px', boxShadow: '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)' }} borderless>
              <Menu.Item style={{ margin: '0px auto' }} vericalAlign="middle">
                <Image src={logo} style={{ margin: 'auto 4px' }} size="mini" />
-               <h2 style={{ color: '#1e5799', margin: '16px 4px 0px' }}>AlagaChuber</h2>
+               <h2 style={{ color: '#4267b2', margin: '16px 4px 0px' }}>alagar.io</h2>
              </Menu.Item>
            </Menu>
            <div style={{
- background: 'linear-gradient(to right bottom, #1e5799 0%,#7db9e8 100%)', height: '700px', display: 'flex', justifyContent: 'center', flexDirection: 'column'
+ background: 'linear-gradient(to right bottom, #4267b2 0%,#7db9e8 100%)', height: '700px', display: 'flex', justifyContent: 'center', flexDirection: 'column'
 }}
            >
              <Container textAlign="center">
                <Container text>
-                 <h2 style={{ color: '#ffffff', marginBottom: '32px' }}>Olá jovem forasteiro, o AlagaChuber é uma plataforma que permite você saber se as ruas da sua região irão alagar hoje.</h2>
+                 <h2 style={{ color: '#ffffff', marginBottom: '32px' }}>Olá jovem forasteiro, o alagar.io é uma plataforma que permite você saber se as ruas da sua região irão alagar hoje.</h2>
                  <p style={{ color: '#ffffff', marginBottom: '64px' }}>Preencha o formulário abaixo e receba as informações diariamente no seu celular ou email. E sempre que quiser pode acessar e consultar a probabilidade</p>
                </Container>
                <Grid stackable>
-                 <Grid.Row columns={2} textAlign="center">
+                 <Grid.Row columns={2} textAlign="center" >
                    {sectionForms.map((field) => {
                   if (field.type === 'Text') {
                     return (
@@ -295,7 +256,7 @@ class HomePage extends Component {
                        circular
                        onClick={this.buttonSubmit}
                        style={{
-backgroundColor: '#1e5799', height: '48px', margin: '0px auto', marginTop: '32px', color: '#ffffff', width: '200px'
+backgroundColor: '#4267b2', height: '48px', margin: '0px auto', marginTop: '32px', color: '#ffffff', width: '200px'
 }}
                      >Confirmar
                      </Button>
@@ -303,19 +264,21 @@ backgroundColor: '#1e5799', height: '48px', margin: '0px auto', marginTop: '32px
                  >
                    <Modal.Content>
                      <div style={{ textAlign: 'center' }}>
-                       {!!this.state.probability &&
-                       <div>
-                         <Image src={feliz} style={{ margin: '32px auto' }} />
-                         <p>BIIIRL!!! Hoje não haverão enchentes na sua região</p>
-                       </div>}
                        {!this.state.probability &&
                        <div>
+                         <Image src={feliz} style={{ margin: '32px auto' }} />
+                         <h2>Hoje não haverá enchentes na sua região.</h2>
+                         <p>Seu dados foram cadastrados, e você receberá email e sms quando as suas regiões de interesse tiverem chances de alagar</p>
+                       </div>}
+                       {!!this.state.probability &&
+                       <div>
                          <Image src={triste} style={{ margin: '32px auto' }} />
-                         <h2>NÃO VAI DA NÃO!!! Sua região tem altas chances de alagar</h2>
+                         <h2>Hoje sua região tem altas chances de alagar.</h2>
+                         <p>Seu dados foram cadastrados, e você receberá email e sms quando as suas regiões de interesse tiverem chances de alagar</p>
                        </div>}
                        <Button
                          style={{
-  backgroundColor: '#1e5799', height: '48px', margin: '0px auto', marginTop: '32px', color: '#ffffff', width: '100px'
+  backgroundColor: '#4267b2', height: '48px', margin: '0px auto', marginTop: '32px', color: '#ffffff', width: '100px'
   }}
                          onClick={() => this.setState({ open: false })}
                          circular
